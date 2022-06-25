0033015972ae038eb30fde9e1d5cc7eee177f68f
Szymon Kosman <szymonkosman@onet.pl>
2022-06-25 19:46:54 +0200

    set semicolon on true in prettier

--- css.d.ts
+++ css.d.ts
@@ -1,11 +1,11 @@
 declare module '*.scss' {
-    const css: { [key: string]: string }
-    export default css
+    const css: { [key: string]: string };
+    export default css;
 }
 declare module '*.sass' {
-    const css: { [key: string]: string }
-    export default css
+    const css: { [key: string]: string };
+    export default css;
 }
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
