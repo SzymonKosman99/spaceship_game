ff911f013ebc73d855591cb9150333c264a976bb
Szymon Kosman <szymonkosman@onet.pl>
2022-06-29 16:12:10 +0200

    added sounds and and new page with settings section

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const infoRouter = require('./routes/info');
+const settingsRouter = require('./routes/settings');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('^/$|/index(.html)?', indexRouter);
 app.use('/info(.html)?', infoRouter);
+app.use('/settings(.html)?', settingsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
