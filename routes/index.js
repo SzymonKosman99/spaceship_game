918ca28f62daca6fd032d36d309c98f27a0a191f
Szymon Kosman <szymonkosman@onet.pl>
2022-07-01 11:51:06 +0200

    set cookies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('start', { title: 'Express' });
+    const { muted_click, muted_background, spaceship_red } = req.cookies;
+    if (
+        muted_click === undefined ||
+        muted_background === undefined ||
+        spaceship_red === undefined
+    ) {
+        res.cookie('muted_click', 'inactive');
+        res.cookie('muted_background', 'inactive');
+        res.cookie('spaceship_red', 'inactive');
+
+        return res.render('start', {
+            title: 'Spaceship game',
+            muted_click: 'inactive',
+            muted_background: 'inactive',
+            spaceship_red: 'inactive',
+        });
+    }
+
+    res.render('start', { title: 'Spaceship game' });
 });
 
 module.exports = router;
