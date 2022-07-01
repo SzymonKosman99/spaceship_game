fd2f6863edd0675cd0b5b598333bca7673379186
Szymon Kosman <szymonkosman@onet.pl>
2022-07-01 13:57:10 +0200

    switch that supports cookie change

--- routes/settings.js
+++ routes/settings.js
@@ -5,4 +5,25 @@ router.get('/', (req, res) => {
     res.render('settings');
 });
 
+router.post('/', (req, res) => {
+    const { setting, value } = req.body.data;
+    if (setting && value) {
+        switch (setting) {
+            case 'muted_click':
+                res.cookie(setting, value);
+                break;
+            case 'muted_background':
+                res.cookie(setting, value);
+                break;
+            case 'spaceship_red':
+                res.cookie(setting, value);
+                break;
+
+            default:
+                break;
+        }
+    }
+    res.json({ message: 'ok' });
+});
+
 module.exports = router;
