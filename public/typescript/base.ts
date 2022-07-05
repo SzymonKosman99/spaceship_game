81aefe6ccd77582320c68a12ccbd4e44adc24ea1
Szymon Kosman <szymonkosman@onet.pl>
2022-07-05 14:49:56 +0200

    changed path to images and transfered data-product to li element in shop section

--- public/typescript/base.ts
+++ public/typescript/base.ts
@@ -1,19 +1,22 @@
 export const DOMElements = {
-    clickSound: document.querySelector<HTMLAudioElement>('[data-click]'),
+    arrowPrev: document.querySelector<HTMLDivElement>('.slider__prev'),
+    arrowNext: document.querySelector<HTMLDivElement>('.slider__next'),
     buttons: Array.from(document.querySelectorAll<HTMLButtonElement>('.btn')),
-    navButtons: Array.from(
-        document.querySelectorAll<HTMLButtonElement>('.btn-nav')
-    ),
+    clickSound: document.querySelector<HTMLAudioElement>('[data-click]'),
     checkboxs: Array.from(
         document.querySelectorAll<HTMLInputElement>('[data-checkbox]')
     ),
-    arrowPrev: document.querySelector<HTMLDivElement>('.slider__prev'),
-    arrowNext: document.querySelector<HTMLDivElement>('.slider__next'),
+    dots: Array.from(document.querySelectorAll<HTMLSpanElement>('.dot')),
+    navButtons: Array.from(
+        document.querySelectorAll<HTMLButtonElement>('.btn-nav')
+    ),
+    products: Array.from(
+        document.querySelectorAll<HTMLImageElement>('[data-product] img')
+    ),
     slides: Array.from(
         document.querySelectorAll<HTMLDivElement>('.slider__content')
     ),
-    dots: Array.from(document.querySelectorAll<HTMLSpanElement>('.dot')),
 };
 
-export type SettingsType = 'muted_click' | 'muted_background' | 'spaceship_red';
 export type IsActive = 'active' | 'inactive';
+export type SettingsType = 'muted_click' | 'muted_background' | 'spaceship_red';
