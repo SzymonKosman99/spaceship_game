64f5368d4969a3372357ab2abda577528eb273f3
Szymon Kosman <szymonkosman@onet.pl>
2022-07-09 21:43:25 +0200

    new variables in game route and pasted them into shop.pug

--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const spaceship_big_price = 5000;
+const spaceship_medium_price = 5000;
+const space_mine_price = 1000;
+const destroyer_missle_price = 7000;
+
 router.get('/shop', (req, res) => {
-    res.render('shop');
+    res.render('shop', {
+        spaceship_big_price,
+        spaceship_medium_price,
+        space_mine_price,
+        destroyer_missle_price,
+    });
 });
 router.put('/shop', (req, res) => {
     const { cookieName, value } = req.body;
