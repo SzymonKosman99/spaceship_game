0d127c70ba6773a4bd8bbfa33c9f7da2bb1fa528
Szymon Kosman <szymonkosman@onet.pl>
2022-07-11 13:42:21 +0200

    new page

--- routes/game.js
+++ routes/game.js
@@ -6,6 +6,10 @@ const spaceship_medium_price = 5000;
 const space_mine_price = 1000;
 const destroyer_missle_price = 7000;
 
+router.get('/', (req, res) => {
+    res.render('field');
+});
+
 router.get('/shop', (req, res) => {
     res.render('shop', {
         spaceship_big_price,
