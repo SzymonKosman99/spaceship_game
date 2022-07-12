16c13def06c4b3a5fe05c961d495715f4b8c00e8
Szymon Kosman <szymonkosman@onet.pl>
2022-07-12 19:24:44 +0200

    added moving and shoting to player spaceship

--- public/typescript/game/PlayerBullet.ts
+++ public/typescript/game/PlayerBullet.ts
@@ -1,32 +1,82 @@
 import Bullet from './abstract/Bullet';
-import { BulletClass, ExplosionClass, ElementPosition } from '../base';
+
+import {
+    DOMElements,
+    BulletClass,
+    ExplosionClass,
+    ElementPosition,
+} from '../base';
 
 class PlayerBullet extends Bullet {
     constructor(
         protected bulletClass: BulletClass,
         protected explosionClass: ExplosionClass,
         protected speed: number,
-        protected bullet: HTMLDivElement
+        protected bullet: HTMLDivElement = document.createElement('div'),
+        private translatePositionX = 0,
+        private translatePositionY = 0
     ) {
         super();
     }
-    public init(spaceshipPosition: ElementPosition): void {}
 
-    protected setPosition(elementPosition: ElementPosition): void {}
+    public init(spaceshipPosition: ElementPosition): void {
+        DOMElements.gameField.appendChild(this.bullet);
+        this.bullet.setAttribute('class', this.bulletClass);
+        this.setPosition({ ...spaceshipPosition });
+    }
 
-    protected movePosition(): void {}
+    protected setPosition(elementPosition: ElementPosition): void {
+        const { edgeRight, edgeLeft } = elementPosition;
+        const spaceshipHeight = edgeRight - edgeLeft;
+        const spaceshipMiddle = spaceshipHeight / 2 + edgeLeft;
+        this.bullet.style.position = 'absolute';
+        this.bullet.style.bottom = `${spaceshipHeight}px`;
+        this.bullet.style.left = `${spaceshipMiddle}px`;
+        this.bullet.style.zIndex = '2';
+        this.translatePositionX = -this.bullet.offsetWidth / 2;
+        this.setTranslatePositions();
+    }
 
     public getPosition(): ElementPosition {
         return {
-            edgeLeft: 0,
-            edgeRight: 0,
-            bottomEdge: 0,
+            edgeLeft: this.bullet.offsetLeft + this.translatePositionX,
+            edgeRight:
+                this.bullet.offsetLeft +
+                this.bullet.offsetWidth +
+                this.translatePositionX,
+            topEdge: this.bullet.offsetTop + this.translatePositionY,
         };
     }
 
-    public explode(): void {}
+    public explode(): void {
+        clearInterval(this.updateTranslatePositionY);
+        this.bullet.classList.remove(this.bulletClass);
+        this.bullet.classList.add(this.explosionClass);
+        this.translatePositionY -= this.bullet.offsetWidth / 2;
+        this.translatePositionX = -this.bullet.offsetWidth / 2;
+        this.setTranslatePositions();
+        setTimeout(() => {
+            this.bullet.remove();
+        }, 600);
+    }
 
-    public remove(): void {}
+    private setTranslatePositions() {
+        this.bullet.style.transform = `translate(${this.translatePositionX}px,${this.translatePositionY}px)`;
+    }
+
+    protected movePosition(): void {
+        this.translatePositionY -= this.speed;
+        this.setTranslatePositions();
+    }
+
+    private updateTranslatePositionY = setInterval(() => {
+        requestAnimationFrame(this.movePosition.bind(this));
+    }, 10);
+
+    public remove(): void {
+        clearInterval(this.updateTranslatePositionY);
+        this.bullet.remove();
+    }
 }
 
 export default PlayerBullet;
--- public/typescript/game/PlayerSpaceship.ts
+++ public/typescript/game/PlayerSpaceship.ts
@@ -1,10 +1,16 @@
-import Bullet from './abstract/Bullet';
-import { DOMElements } from '../base';
 import Spaceship from './abstract/Spaceship';
-
 import PlayerBullet from './PlayerBullet';
+import State from '../State';
 
-import { SpaceshipClass, ExplosionClass, ElementPosition } from '../base';
+import {
+    DOMElements,
+    SpaceshipClass,
+    ExplosionClass,
+    ElementPosition,
+    BulletClass,
+} from '../base';
+
+const { blastSound } = DOMElements;
 
 class PlayerSpaceship extends Spaceship {
     constructor(
@@ -12,7 +18,11 @@ class PlayerSpaceship extends Spaceship {
         protected explosionClass: ExplosionClass,
         protected lives: number,
         protected speed: number,
-        protected spaceship: HTMLDivElement
+        protected spaceship: HTMLDivElement = document.createElement('div'),
+        private isArrowLeftPressed = false,
+        private isArrowRightPressed = false,
+        private translatePositionX = 0,
+        private counter = 0
     ) {
         super();
     }
@@ -21,37 +31,121 @@ class PlayerSpaceship extends Spaceship {
         DOMElements.gameField.appendChild(this.spaceship);
         this.spaceship.classList.add(this.spaceshipClass);
         this.setPosition();
-        this.checkPosition();
+        this.addEvents();
     }
 
     protected setPosition(): void {
         this.spaceship.style.position = 'absolute';
-        this.spaceship.style.left = `${
-            window.innerWidth / 2 - this.spaceship.clientWidth / 2
-        }px`;
-        this.spaceship.style.top = `${
-            window.innerHeight - this.spaceship.clientHeight
-        }px`;
-    }
+        this.spaceship.style.left = `${window.innerWidth / 2}px`;
+        this.spaceship.style.bottom = `0px`;
 
-    protected movePosition(): number | void {}
+        this.translatePositionX = -this.spaceship.offsetWidth / 2;
+        this.spaceship.style.transform = `translateX(${this.translatePositionX}px)`;
+    }
 
     protected getPosition(): ElementPosition {
         return {
-            edgeLeft: 0,
-            edgeRight: 0,
-            bottomEdge: 0,
+            edgeLeft: this.spaceship.offsetLeft + this.translatePositionX,
+            edgeRight:
+                this.spaceship.offsetLeft +
+                this.spaceship.offsetWidth +
+                this.translatePositionX,
+            topEdge: this.spaceship.offsetTop,
         };
     }
-    public explode(): void {}
 
-    public remove(): void {}
+    protected movePosition(): number | void {
+        const elementPosition = this.getPosition();
+        const { edgeLeft, edgeRight } = elementPosition;
 
-    private checkPosition() {
-        console.log(this.spaceship.clientHeight, this.spaceship.clientWidth);
-        requestAnimationFrame(() => {
-            this.checkPosition;
-        });
+        if (this.isArrowLeftPressed && edgeLeft >= 0) {
+            this.translatePositionX -= this.speed;
+            this.spaceship.style.transform = `translateX(${this.translatePositionX}px)`;
+        }
+        if (this.isArrowRightPressed && edgeRight <= window.innerWidth) {
+            this.translatePositionX += this.speed;
+            this.spaceship.style.transform = `translateX(${this.translatePositionX}px)`;
+        }
+    }
+
+    private checkIsButtonPressed = setInterval(() => {
+        requestAnimationFrame(this.movePosition.bind(this));
+    }, 10);
+
+    private addEvents(): void {
+        document.addEventListener('keydown', this.handleKeyDown);
+        document.addEventListener('keyup', this.handleKeyUp);
+    }
+
+    private handleKeyDown = (e: KeyboardEvent) => {
+        switch (e.key) {
+            case 'ArrowLeft':
+                this.isArrowLeftPressed = true;
+                break;
+            case 'ArrowRight':
+                this.isArrowRightPressed = true;
+                break;
+            case ' ':
+                if (!blastSound) {
+                    blastSound.pause();
+                }
+                break;
+            default:
+                break;
+        }
+    };
+
+    private handleKeyUp = (e: KeyboardEvent) => {
+        switch (e.key) {
+            case 'ArrowLeft':
+                this.isArrowLeftPressed = false;
+                break;
+            case 'ArrowRight':
+                this.isArrowRightPressed = false;
+                break;
+            case ' ':
+                this.shot();
+                if (blastSound.paused) {
+                    blastSound.load();
+                    blastSound.play();
+                }
+                break;
+            default:
+                break;
+        }
+    };
+
+    private shot() {
+        const color = State.spaceship_red === 'active' ? '_red' : '_blue';
+        const bulletClass = `bullet${color}` as BulletClass;
+        const playerBullet = new PlayerBullet(
+            bulletClass,
+            this.explosionClass,
+            6,
+            document.createElement('div')
+        );
+        playerBullet.init(this.getPosition());
+    }
+
+    public explode(): void {
+        clearInterval(this.checkIsButtonPressed);
+        this.removeEvents();
+        this.spaceship.classList.remove(this.spaceshipClass);
+        this.spaceship.classList.add(this.explosionClass);
+        setTimeout(() => {
+            this.spaceship.remove();
+        }, 600);
+    }
+
+    public remove(): void {
+        clearInterval(this.checkIsButtonPressed);
+        this.removeEvents();
+        this.spaceship.remove();
+    }
+
+    private removeEvents() {
+        document.removeEventListener('keydown', this.handleKeyDown);
+        document.removeEventListener('keyup', this.handleKeyUp);
     }
 }
 
