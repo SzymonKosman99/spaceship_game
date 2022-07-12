7cec88c8e9b5a0db0e67e30144c2d45970bb1d58
Szymon Kosman <szymonkosman@onet.pl>
2022-07-12 14:17:00 +0200

    added to start function check which params add to create spaceship

--- public/typescript/game/Game.ts
+++ public/typescript/game/Game.ts
@@ -1,5 +1,46 @@
+import { ExplosionClass, SpaceshipClass } from '../base';
+import State from '../State';
+import PlayerSpaceship from './PlayerSpaceship';
+
 class Game {
-    public start() {}
+    public start() {
+        const { spaceship_red, spaceship_model } = State;
+        const color = spaceship_red === 'active' ? '_red' : '_blue';
+        const model = spaceship_model;
+
+        const spaceshipClass = (model + color) as SpaceshipClass;
+
+        const explosionClass = (
+            model === 'spaceship_big'
+                ? `explosion_big${color}`
+                : `explosion_small${color}`
+        ) as ExplosionClass;
+
+        const lives =
+            spaceship_model === 'spaceship_big'
+                ? 10
+                : spaceship_model === 'spaceship_medium'
+                ? 5
+                : 3;
+
+        const speed =
+            spaceship_model === 'spaceship_big'
+                ? 5
+                : spaceship_model === 'spaceship_medium'
+                ? 5.5
+                : 5;
+
+        const div = document.createElement('div');
+
+        const mySpaceship = new PlayerSpaceship(
+            spaceshipClass,
+            explosionClass,
+            lives,
+            speed,
+            div
+        );
+        mySpaceship.init();
+    }
 }
 
 export default Game;
