e9dfed30d05479495a34b7d6812f750055efc6cd
Szymon Kosman <szymonkosman@onet.pl>
2022-07-14 18:20:52 +0200

    created bulletFactory

---
+++ public/typescript/game/BulletsFactory.ts
@@ -0,0 +1,28 @@
+import { BulletClass, ElementPosition, ExplosionClass } from '../base';
+
+import EnemyBullet from './EnemyBullet';
+import State from '../State';
+import PlayerBullet from './PlayerBullet';
+
+class BulletFactory {
+    public static createBullet(
+        bulletClass: BulletClass,
+        explosionClass: ExplosionClass,
+        speed: number,
+        spaceshipPosition: ElementPosition
+    ) {
+        if (bulletClass.includes('--rotated')) {
+            const bullet = new EnemyBullet(bulletClass, explosionClass, speed);
+            bullet.init(spaceshipPosition);
+            State.gameState.enemy_bullets.push(bullet);
+            console.log(State.gameState.enemy_bullets);
+        } else {
+            const bullet = new PlayerBullet(bulletClass, explosionClass, speed);
+            bullet.init(spaceshipPosition);
+            State.gameState.player_bullets.push(bullet);
+            console.log(State.gameState.player_bullets);
+        }
+    }
+}
+
+export default BulletFactory;
