5bd2209af94ceef8c80afaa3fc97f5a22dc7be35
Szymon Kosman <szymonkosman@onet.pl>
2022-07-18 13:19:50 +0200

    changed block of last condition

--- public/typescript/index.ts
+++ public/typescript/index.ts
@@ -53,25 +53,7 @@ window.onload = () => {
     }
 
     if (DOMElements.gameField) {
-        const myButtons = new Buttons();
         const myGame = new Game();
-
-        myButtons.init();
         myGame.start();
     }
-
-    //     const message = `
-    //     <div class="modal">
-    //       <h3 class='warning'>Open this page in desktop device</h3>
-    //       <p class='warning'>Sorry but this page is available only for desktop devices :(</p>
-    //     </div>
-    //    `;
-
-    // FIXME: const isDesktopDevice =
-    //     window.innerWidth >= 1280 && window.innerHeight >= 648;
-    // if (isDesktopDevice) {
-    // } else {
-    //     document.body.innerHTML = '';
-    //     document.body.innerHTML = message;
-    // }
 };
